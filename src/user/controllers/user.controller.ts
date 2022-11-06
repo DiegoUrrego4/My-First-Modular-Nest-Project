282ed17bb7b96a2c5ef696d87c8704085559e983
Diego Urrego <diego.urrego@sofka.com.co>
2022-11-06 14:58:43 -0500

    Instalación de paquetes necesarios, creación de la interfaz user y se terminan las funcionalidades de obtener todos los usuarios y un usuario por ID.

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private usersService: UserService) {}
-  @Get('message')
-  getSaludo(): string {
-    return this.usersService.getSaludo();
+  @Get()
+  getAllUsers() {
+    return this.usersService.findAll();
+  }
+  @Get(':uuid')
+  getUser(@Param('uuid') id: string) {
+    return this.usersService.findOneById(id);
   }
 }
--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { User } from '../interfaces/users.interface';
 
 @Injectable()
 export class UserService {
-  getSaludo(): string {
-    return 'Hola desde el servicio Users';
+  private users: User[] = [
+    {
+      uuid: uuid(),
+      nombre: 'Diego',
+      apellido: 'Urrego',
+      email: 'diegoUrrego@mail.com',
+    },
+    {
+      uuid: uuid(),
+      nombre: 'Julián',
+      email: 'julianLasso@mail.com',
+    },
+    {
+      uuid: uuid(),
+      nombre: 'John',
+      apellido: 'Salazar',
+      email: 'johnSalazar@mail.com',
+    },
+  ];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOneById(uuid: string) {
+    const user = this.users.find((user) => user.uuid === uuid);
+    if (!user)
+      throw new NotFoundException(`No existe un usario con uuid:${uuid}`);
+    return user;
   }
 }
